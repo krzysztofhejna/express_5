f728e6d13b46092c1d474f71e745b6f652467e13
krzysztofhejna <khejna15@wp.pl>
2019-02-18 16:46:38 +0100

    Enable google authentification

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  'GOOGLE_CLIENT_ID': '434899961624-lpdi0lm9mmbopmtbu65187j1broir9ep.apps.googleusercontent.com',
+  'GOOGLE_CLIENT_SECRET': 'qpuZK-pVqkjFiYG0DwVCTuFv',
+  'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
